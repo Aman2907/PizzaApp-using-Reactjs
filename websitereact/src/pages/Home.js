985b455a6934b04c1bd436ae4ab94fbd310fae17
Aman Sharma <74008888+Aman2907@users.noreply.github.com>
2023-09-08 14:11:57 +0530

    Update Home.js

--- websitereact/src/pages/Home.js
+++ websitereact/src/pages/Home.js
@@ -4,10 +4,11 @@ import BannerImage from '../photo/pizza.jpeg'
 import '../Style/Home.css'
 
 export function Home() {
-  return (
+  return ( 
     <div className='home' style={{ backgroundImage: `url(${BannerImage})`}}>
       <div className='headerContainer' > 
         <h1>  Pizza Store </h1>
+  
         <p>  Pizza to fit any taste </p>  
         <Link to='/menu'>
         <div className='butt'>

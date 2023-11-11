24ab6c55d12197da705f2b76dd038693b93c44cc
Aman Sharma <74008888+Aman2907@users.noreply.github.com>
2023-11-11 20:13:10 +0530

    Update MenuItem.js

--- websitereact/src/components/MenuItem.js
+++ websitereact/src/components/MenuItem.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
 
- 
+  
 function MenuItem({ image, name, price }) {
   return (
     <div className='menuItem'>
+   
         <div style={{ backgroundImage: `url(${image})` }}> </div>
         <h1> {name} </h1>
         <p> <CurrencyRupeeIcon/>{price} </p>
